56d69aadafb92d52c15a0b56fc85550d8eea454d
Robert Ligman <verxus123@gmail.com>
2021-03-18 14:29:48 +0100

    add react-transition-group to ListItem

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,16 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { CSSTransition } from "react-transition-group";
 import "./ListItem.css";
-const ListItem = (props) => (
-  <li
-    className={`task-list__item ${props.isAccomplished ? "accomplished" : ""}`}>
-    {props.name}
-    <button className="btn btn-success" onClick={props.clickCheck}>
-      <FontAwesomeIcon icon={faCheck} />
-    </button>
-    <button className="btn btn-danger" onClick={props.click}>
-      <FontAwesomeIcon icon={faTrash} />
-    </button>
-  </li>
-);
+import { useRef } from "react";
+const ListItem = (props) => {
+  const nodeRef = useRef(null);
+  return (
+    <CSSTransition
+      in={true}
+      classNames="my-node"
+      appear={true}
+      timeout={1000}
+      nodeRef={nodeRef}>
+      <li
+        ref={nodeRef}
+        className={`task-list__item ${
+          props.isAccomplished ? "accomplished" : ""
+        }`}>
+        {props.name}
+        <button className="btn btn-success" onClick={props.clickCheck}>
+          <FontAwesomeIcon icon={faCheck} />
+        </button>
+        <button className="btn btn-danger" onClick={props.click}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </li>
+    </CSSTransition>
+  );
+};
 export default ListItem;
